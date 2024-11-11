5a52b8f3ea055de34c4d845192edab8ba365a289
Pranav sirsufale <pranavdada334@gmail.com>
2024-11-11 18:18:06 +0530

    added more visualizations

--- visualization.R
+++ visualization.R
@@ -139,5 +139,33 @@ points(x = df$lat[1:500], y = df$lng[1:500], col = "Red")
 
 
 
+# Adding Titles and Labeling Axes to Plot
+cone <- function(x, y){
+  sqrt(x ^ 2 + y ^ 2)
+}
+
+# prepare variables.
+x <- y <- seq(-1, 1, length = 30)
+z <- outer(x, y, cone)
+
+# plot the 3D surface
+# Adding Titles and Labeling Axes to Plot
+persp(x, y, z,
+      main="Perspective Plot of a Cone",
+      zlab = "Height",
+      theta = 30, phi = 15,
+      col = "orange", shade = 0.4)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
