c0fa80dca114afda71809ad53d36a8706bfa9a9c
Pranav sirsufale <pranavdada334@gmail.com>
2024-11-12 18:50:35 +0530

    perform data manipulation operation with data

--- data_manipulaiton_with_realdata.R
+++ data_manipulaiton_with_realdata.R
@@ -22,6 +22,7 @@ nums
 bamu %>% select(1:100) -> bamu2
 
 View(bamu2)
+View(bamu)
 
 
 
@@ -37,6 +38,32 @@ column_name_endswith <- bamu %>% select(ends_with("s"))
 
 ncol(column_name_endswith)
 View(column_name_endswith)
+
+phd_cs <- bamu %>% filter(bamu$`PROGRAMME NAME` == 'DOCTOR OF PHILOSOPHY(COMPUTER SCIENCE)')
+
+View(phd_cs)
+
+phd_all <- bamu %>% filter(grepl('DOCTOR OF PHILOSOPHY', bamu$`PROGRAMME NAME`))
+
+
+View(phd_all)
+
+# Export a data frame to a text file using write.csv()
+df = data.frame(phd_cs) 
+
+
+nrow(phd_all)
+nrow(phd_cs)
+ncol(phd_cs)
+ncol(phd_all)
+
+write.csv(df, file = "F:/Data science/Data analysis/filtered/phd_cs.csv")
+write.csv(phd_all, file = "F:/Data science/Data analysis/filtered/phd_all.csv")
+
+library(ggplot2)
+
+
+
 
 
 
