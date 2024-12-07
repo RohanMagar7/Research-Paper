947c12654f61ced2345bc8bf8e75b3d897996ea4
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-07 19:16:47 +0530

    add scripts

---
+++ data cleaning/ug colleges/college.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+
+bamu
+
+# Check unique college names
+unique_colleges <- unique(bamu$UG.INSTITUTE.COLLEGE)
+length(unique_colleges)  
+nrow(unique_colleges)
+ncol(unique_colleges)
+length(unique_colleges)
+view(unique_colleges)
