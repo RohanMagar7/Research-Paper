89514b5e8d32a5bea224c66e877a968863f70446
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-18 09:27:28 +0530

    add R script

--- data cleaning/ug colleges/learning.R
+++ data cleaning/ug colleges/learning.R
@@ -6,3 +6,34 @@ expanded_path
 demo()
 
 demo(graphics)
+
+
+
+# getting working directory
+wkdir <- getwd()
+
+
+
+# setting working directory
+#setwd('F:/R programming/data cleaning/ug colleges')
+
+
+getwd()
+
+
+dir()
+
+dir(pattern = '.txt')       
+
+dir(pattern = '.R')   
+
+file.exists('bamudb.R')
+
+newdirPath <- 'createdbycode'
+
+if ( file.exists(wkdir)) dir.create(newdirPath)
+
+dir(pattern = 'create')
+
+# creating a file
+file.create('learning.R')
