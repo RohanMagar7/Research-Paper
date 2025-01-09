24e4cbe07219d2f4fb9ded21b0ce1caface8d5fb
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 12:45:40 +0530

    add R Script code

--- data cleaning/data cleaning/clearning_data.R
+++ data cleaning/data cleaning/clearning_data.R
@@ -19,6 +19,81 @@ genderTable
 colnames(genderTable) <- c('gende','counts')
 
 
+# select variables 
+names(data)
+
+
+
+
+glimpse(starwars)
+
+class(starwars$gender)
+
+unique(starwars$gender)
+
+
+levels(starwars$gender)
+
+
+starwars$gender <- factor((starwars$gender),
+                          levels = c('masculine', 'feminine'))
+
+
+
+
+levels(starwars$gender)
+
+
+
+#### selecting variables
+
+names(starwars)
+
+
+starwars %>%
+  select(name, height, ends_with('color')) %>% # selecting variabes that ends with color
+  names()
+
+
+
+starwars %>%
+  select(name,height,ends_with('color')) %>%
+  filter(hair_color %in% c('blond' , 'brown') &
+           height < 180 )
+
+
+
+phdcs_chemi <- data %>%
+  select( PROGRAMME.NAME, NAME.OF.THE.APPLICANT, CATEGORY , starts_with('DOCTOR OF PHILOSOPHY')) %>%
+  filter(PROGRAMME.NAME %in% c('DOCTOR OF PHILOSOPHY(COMPUTER SCIENCE)'))
+
+
+
+
+view(phdcs_chemi)
+
+
+
+table(phdcs_chemi$CATEGORY)
+
+
+view(table(phdcs_chemi$CATEGORY))
+
+
+barplot(table(phdcs_chemi$CATEGORY))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
