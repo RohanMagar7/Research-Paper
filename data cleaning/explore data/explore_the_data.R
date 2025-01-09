b8838b1898539d89e229641c6458291760122da2
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 10:02:14 +0530

    add R Script code

--- data cleaning/explore data/explore_the_data.R
+++ data cleaning/explore data/explore_the_data.R
@@ -21,6 +21,49 @@ glimpse(data)
 # in the table format
 view(data)
 
+# first 6 rows 
+head(data)
+
+# last 6 rows 
+tail(data)
+
+
+attach(data)
+
+
+# all the column names
+names(data)
+
+
+# row length ( how many columns are there)
+length(data)
+
+# all the gender count (18736)
+length(data$GENDER)
+
+# unique gender 
+unique(data$GENDER)
+# Male , FEMALE , TRANSGENDER 
+
+
+# unique programmes
+unique(data$PROGRAMME.NAME)
+
+
+# count of unique programmes ( 167 )
+length(unique(data$PROGRAMME.NAME))
+
+
+
+
+
+
+
+# data type
+class(data$GENDER)
+
+
+
 
 
 
