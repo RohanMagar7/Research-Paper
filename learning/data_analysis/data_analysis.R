7dcf1bf679d4936ef5ed6519d534f2bfdaff401c
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-03 22:27:22 +0530

    add R script

--- learning/data_analysis/data_analysis.R
+++ learning/data_analysis/data_analysis.R
@@ -17,3 +17,28 @@ stateInfo <- read.csv('F:/R programming/learning/data_analysis/material/stateDat
 # getting data where regino value is 1
 View(stateInfo[stateInfo$state.region == 1, ])
 
+
+
+edit(stateInfo)
+
+
+
+
+
+# reading binary data from a BMP file 
+
+image.file <- "C:/Users/HP/Downloads/man.png"
+
+
+myimage.info <- file.info(image.file)
+
+
+myimage.info
+myimage.data <- readBin(image.file, 
+                        n = as.numeric(myimage.info['size']),
+                        what = 'raw'
+                        )
+
+
+myimage.data
+
