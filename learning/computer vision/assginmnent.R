ef2cf3b2be85b5af6adb08a218700b51195f6856
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-08 12:56:15 +0530

    add R programme

--- learning/computer vision/assginmnent.R
+++ learning/computer vision/assginmnent.R
@@ -20,14 +20,35 @@ colnames(sepalwidth) <- c('sepalwidth','frequency')
 
 View(sepalwidth)
 
+hist(iris$Sepal.Width)
 
 ggplot(sepalwidth,aes(x=`sepalwidth`,y=`frequency`))+
   geom_histogram(stat = 'identity', binwidth = 5, bins = 2) +
   labs(title = 'Sepal width Distribution', x = 'Sepal Width' , y = 'Frequency', fill ='Frequency')
 
   
+ggplot(iris, aes(x = Sepal.Width)) +
+  geom_histogram(binwidth = 0.2, color = 'black') +
+  labs(title = 'Sepal Width Distribution', x = 'Sepal Width', y = 'Frequency')
+
+  
+
+ggplot(sepalwidth, aes(x = sepalwidth, y = frequency)) +
+  geom_bar(stat = 'identity', fill = 'green', width = 1) +
+  labs(title = 'Sepal Width Distribution', x = 'Sepal Width', y = 'Frequency')
+
   
   
-  
-  
+pl <- iris$Petal.Length
+pw <- iris$Petal.Width
+
+plot(pl)
+plot(pw)
+plot(pl,pw)
+
+plot(pw,pl)
+
+
+
+
   
