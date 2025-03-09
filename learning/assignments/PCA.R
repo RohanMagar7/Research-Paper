99931e7125255a593127ae8038001c8b14c36379
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-10 02:36:58 +0530

    add R script

--- learning/assignments/PCA.R
+++ learning/assignments/PCA.R
@@ -29,3 +29,24 @@ ggplot(data.frame(pc = factor(1:4) , variance = prop_variance) , aes(x = pc, y =
   theme_minimal()
 
 
+
+
+pca_df <- data.frame(pca_result$x , Species = iris$Species)
+
+ggplot(pca_df , aes(x = PC1 , y = PC2 , color = Species)) + 
+  geom_point(size = 3 ) + 
+  ggtitle("PcA : First two components ") + 
+  theme_minimal()
+
+
+plot_ly(pca_df , x = ~ PC1 , y = ~PC2 , z = ~PC3, color = ~Species , colors = c('red','blue','green')) %>%
+  add_markers() %>%
+  layout(title = "3D PCA visualization",
+         scene = list(xaxis = list(xasix = list(title = 'pc1') , yaxis = list(title = 'pc2'), zaxis = list(title = 'pc3')))
+         )
+
+
+
+
+
+
