d8549255625cae203b2f40fd8cc3d32d5746f852
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-09 21:56:00 +0530

    add r programme

---
+++ learning/assignments/DECISION_TREE.R
@@ -0,0 +1,29 @@
+library(caTools)
+library(rpart)
+
+dt <- read.csv(path)
+dt <- dt[3:5]
+dt[,1:2] <- scale(dt[,1:2])
+
+dt$Purchased <- factor(dt$Purchased , levels = c(0,1))
+
+set.seed(123)
+split <- sample.split(dt$Purchased , SplitRatio =  0.75)
+dt.train <- subset(dt , split == TRUE)
+dt.test <- subset(dt,split == FALSE)
+
+
+
+dtclassifier <- rpart(formula = Purchased ~ .,data = dt.train)
+
+
+
+
+
+
+
+
+
+
+
+
