c8e6b6863d6d0c3c27ebdc91aa426aa765522f11
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-10 00:11:02 +0530

    add r programme

--- learning/assignments/K_MEANS.R
+++ learning/assignments/K_MEANS.R
@@ -1,5 +1,27 @@
+library(ggplot2)
+iris_data <- iris[,-5]
+
+# apply k - means 
+set.seed(123)
+# perform k - means 
+kmeans_result <- kmeans(iris_data , centers = 3 , nstart = 25)
+
+# visualize the cluster
+
+iris$cluster <- as.factor(kmeans_result$cluster)
 
 
+View(iris)
+
+# scatter plot of clusters 
+ggplot(iris,aes(x = Sepal.Length, y = Sepal.Width , colour = cluster)) + 
+  geom_point(size = 3 ) + 
+  labs(
+    title = 'K-Means clustering of Iris Dataset',
+    x = 'Sepal Length ',
+    y = "sepal width"
+      ) + 
+  theme_minimal()
 
 
 
