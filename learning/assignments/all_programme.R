aca0244b5babc70ea328d16f3f72cc3c65d2f1b1
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-10 07:29:49 +0530

    add R script

--- learning/assignments/all_programme.R
+++ learning/assignments/all_programme.R
@@ -1,23 +1,10 @@
-
-get_center <- function(dat){
-  mean_value = mean(dat)
-  median_value = median(dat)
-  mode_value = names(which.max(table(data)))
-  
-  cat("Mean:", mean_value, "\n")
-  cat("Median:", median_value, "\n")
-  cat("Mode:", mode_value, "\n")
-  
-}
-data <- c(10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80)
-get_center(data)
-
-
-
-
+mode_value = names(which.max(table(data)))
 ###@@@@
 e1071
 
+# Define colors for species
+species_colors <- c("setosa" = "red", "versicolor" = "blue", "virginica" = "green")
 
-
-
+# Add legend
+legend("topright", legend=names(species_colors), 
+       col=species_colors, pch=19, title="Species")
