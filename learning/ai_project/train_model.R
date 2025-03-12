1113fbd108177067cf94251e0bdbbb38e5e3531a
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-12 21:17:55 +0530

    add R programme

--- learning/ai_project/train_model.R
+++ learning/ai_project/train_model.R
@@ -1,3 +1,4 @@
+setwd("F:/R programming/learning/ai_project")
 # Load libraries
 library(mlbench)
 library(caret)
@@ -11,11 +12,17 @@ df <- PimaIndiansDiabetes
 # Split into training & testing sets
 set.seed(123)
 trainIndex <- sample.split(df$diabetes, SplitRatio = 0.90)
+
 train <- subset(df, trainIndex)
 test <- subset(df, !trainIndex)
+
+head(train)
+summary(train)
 
 # Train Random Forest model
 model <- randomForest(diabetes ~ ., data = train, ntree = 100)
 
+
 # Save model for deployment
 saveRDS(model, "diabetes_model.rds")
+
