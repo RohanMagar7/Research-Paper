b0bf2e74c31e2a6904ffda916fc8def30185b007
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-12 16:14:00 +0530

    add R script

--- learning/assignments/dendrogram.R
+++ learning/assignments/dendrogram.R
@@ -1,34 +1,30 @@
-library(ggplot2)  # For visualization
+library(ggplot2)  
 library(dendextend)  # For better dendrogram visualization
+
 
 iris.data <- scale(iris[,-5])
 
-### step 3 computer distance metrics  & apply hierachical clustering 
 
+# step 3 compute distance metrics & apply hierachical clustering 
 distance_matrix <- dist(iris.data , method = 'euclidean')
-hc <- hclust(distance_matrix , method = 'complete')
+h.cluster <- hclust(distance_matrix , method = 'complete')
+# step 4 visualize dendrogram 
+plot(h.cluster , main = 'Hierachical clustering Dendrogram',sub = '',xlab = '', cex = 0.6)
 
-# step 4 : visualize dendrogramm 
-# Basic dendrogram
-plot(hc,main='Hierachical clustering Dendrogram' , sub = '' , xlab = '' , cex = 0.6)
+# step 5 improve the dendrogram visuallization ( convert to dendrogram object for customization)
+dend <- as.dendrogram(h.cluster)
 
-
-# step 5 imrove the dendrogramm visualization
-# convert to dendrogram object for customization
-dend <- as.dendrogram(hc)
 
 # plot enhance dendrogram 
 plot(dend, main = 'Colored Dendrogram of Iris Dataset')
 
 
+# step 6 : cut the tree from center 
+clusters <- cutree(h.cluster , k = 3 )
 
-### optional 
-
-# Step 6 : cut the tree from from center 🌴 
-clusters <- cutree(hc, k = 3)
-
-# Add cluster labels to dataset
+# add cluster labels to dataset
 iris$Cluster <- as.factor(clusters)
+
 
 # View first few rows
 head(iris)
@@ -38,7 +34,7 @@ ggplot(iris, aes(x = Sepal.Length , y = Sepal.Width , color = Cluster)) +
   geom_point(size = 3 , alpha = 0.7) + 
   labs( title = 'Hierachical clustering : Sepal Length Vs width',
         x = 'Sepal Length',
-        y = 'Sepal Width') + 
+        y = 'Sepal Width') +
   theme_minimal()
 
 
