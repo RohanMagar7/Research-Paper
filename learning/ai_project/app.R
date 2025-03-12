e12c1d4a12b9657f04052e7c6a630b3a0ec1b9a6
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-12 21:01:04 +0530

    add R programme

--- learning/ai_project/app.R
+++ learning/ai_project/app.R
@@ -1,3 +1,4 @@
+setwd("F:/R programming/learning/ai_project")
 library(shiny)
 library(randomForest)
 
@@ -11,6 +12,13 @@ ui <- fluidPage(
     sidebarPanel(
       numericInput("glucose", "Glucose Level:", value = 100, min = 50, max = 200),
       numericInput("bmi", "BMI:", value = 25, min = 10, max = 50),
+      numericInput("pregnant", "Pregnancies:", value = 2, min = 0, max = 20),
+      numericInput('triceps','Triceps',value = 20 , min = 0 , max = 99),
+      numericInput('insulin' ,'Insulin', value = 77 , min = 0 , max = 846),
+      numericInput('mass','Mass',value = 31 , min = 0 , max = 68),
+      numericInput('pedigree','Pedigree',value = 0.5, min = 0 , max = 3),
+      numericInput('age','Age',value=29,min = 21,max = 81),
+      numericInput('pressure','Pressure',value = 69 , min = 0 , max = 122),
       actionButton("predict", "Predict")
     ),
     mainPanel(
@@ -22,7 +30,10 @@ ui <- fluidPage(
 # Define Server Logic
 server <- function(input, output) {
   predict_diabetes <- eventReactive(input$predict, {
-    new_data <- data.frame(glucose = input$glucose, bmi = input$bmi)
+    new_data <- data.frame(glucose = input$glucose, bmi = input$bmi , pregnant = input$pregnant,
+                           triceps = input$triceps ,insulin = input$insulin , mass = input$mass,
+                           pedigree = input$pedigree , age = input$age, pressure = input$pressure
+                           )
     predict(model, new_data)
   })
   
